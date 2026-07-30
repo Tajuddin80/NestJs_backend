aa668c4baf08f6b61f20e146068b8049ec3e1cc3
Tajuddin80 <tajuddin.cse.dev@gmail.com>
2026-07-30 14:49:22 +0600

    feat: implement ApiKeyMiddleware and integrate it into the application; add unit tests for middleware

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,8 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
 
+  app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(process.env.PORT ?? 3000);
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -4,60 +4,59 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 interface IUser {
-    id: number,
-    name: string,
-    email: string
+  id: number;
+  name: string;
+  email: string;
 }
 @Injectable()
 export class UserService {
-    constructor(private readonly logger: LoggerService
-    ) { }
-    private users: IUser[] = [
-        { id: 1, name: 'tajuddin', email: 'taj@gmail.com' },
-        { id: 2, name: 'uddin', email: 'uddin@gmail.com' }
-    ];
+  constructor(private readonly logger: LoggerService) {}
+  private users: IUser[] = [
+    { id: 1, name: 'tajuddin', email: 'taj@gmail.com' },
+    { id: 2, name: 'uddin', email: 'uddin@gmail.com' },
+  ];
 
-    findAllUsers(name: string = '') {
-        this.logger.log('Finding all users')
+  findAllUsers(name: string = '') {
+    this.logger.log('Finding all users');
 
-        return this.users.filter((user) =>
-            user.name.toLowerCase().includes(name.toLowerCase())
-        )
+    return this.users.filter((user) =>
+      user.name.toLowerCase().includes(name.toLowerCase()),
+    );
+  }
+
+  findOneUser(id: number) {
+    const user = this.users.find((user) => user.id === id) ?? null;
+    if (!user) {
+      throw new NotFoundException('user not found');
     }
+    return user;
+  }
 
-    findOneUser(id: number) {
-        const user = this.users.find((user) => user.id === id) ?? null;
-        if (!user) {
-            throw new NotFoundException('user not found');
-        }
-        return user;
-    }
+  createUser(dto: CreateUserDto) {
+    this.logger.log('creating user');
 
-    createUser(dto: CreateUserDto) {
-        this.logger.log('creating user');
+    const newUser: IUser = { id: this.users.length + 1, ...dto };
+    this.users.push(newUser);
 
-        const newUser: IUser = { id: this.users.length + 1, ...dto };
-        this.users.push(newUser);
+    return newUser;
+  }
 
-        return newUser;
-    }
+  updateUser(id: number, dto: UpdateUserDto) {
+    this.logger.log(`updating user ${id}`);
 
-    updateUser(id: number, dto: UpdateUserDto) {
-        this.logger.log(`updating user ${id}`)
+    const index = this.users.findIndex((user) => user.id === id);
+    if (index === -1) return null;
 
-        const index = this.users.findIndex((user) => user.id === id);
-        if (index === -1) return null;
+    this.users[index] = { ...this.users[index], ...dto };
+    return this.users[index];
+  }
 
-        this.users[index] = { ...this.users[index], ...dto };
-        return this.users[index];
-    }
+  deleteUser(id: number) {
+    this.logger.log(`deleting user ${id}`);
 
-    deleteUser(id: number) {
-        this.logger.log(`deleting user ${id}`)
-
-        const index = this.users.findIndex((user) => user.id === id);
-        if (index === -1) return null;
-        const [deleted] = this.users.splice(index, 1);
-        return deleted;
-    }
+    const index = this.users.findIndex((user) => user.id === id);
+    if (index === -1) return null;
+    const [deleted] = this.users.splice(index, 1);
+    return deleted;
+  }
 }
